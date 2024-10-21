049cae7ffa8896f76e381ff35ed65a47d8d5f722
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-10-22 06:37:08 +0800

    dreamdetail

--- App.js
+++ App.js
@@ -322,6 +322,23 @@ app.get('/triviadetail/:triviaId', async(req,res)=>{
     res.send(output)
 })
 
+
+app.get('/dreamDetail/:dreamId', async(req,res)=>{
+    let cardId=Number(req.params.dreamId);
+
+    if (cardId){
+        query = {
+            "id2": Number(cardId)
+        }
+    }else{
+        query={}
+    }
+
+
+    let collection = "Dream Card";
+    let output = await getData(collection,query);
+    res.send(output)
+})
 app.post('/pickfavcard',async(req,res) => {
     let body = req.body;
     let collection = 'favcard';
